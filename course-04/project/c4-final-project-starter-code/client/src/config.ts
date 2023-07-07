4da81631faf77f6bb612b829cca9d98b7a5c68df
Nguyen Ba Son <nguyenbason@windowslive.com>
2023-07-07 11:11:43 +0700

    update to start CI

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'plj6tna1j0'
+const apiId = 'i0ts197p10'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
